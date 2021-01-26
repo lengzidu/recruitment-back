49de5416a2cd0641eea6278cb22c7256882de9b8
wjj <1605291159@qq.com>
2021-01-26 22:01:48 +0800

    01.26

---
+++ db/db_test.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+// 1.2. 连接指定数据库(URL 只有数据库是变化的)
+mongoose.connect('mongodb://localhost:27017/gzhipin_test2')
+// 1.3. 获取连接对象
+const conn = mongoose.connection
+// 1.4. 绑定连接完成的监听(用来提示连接成功)
+conn.on('connected', function () {
+console.log('数据库连接成功')
+})
