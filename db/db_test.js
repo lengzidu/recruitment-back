2a42648f42304664ae72ee5919318c80de8b7bb9
wjj <1605291159@qq.com>
2021-02-01 23:44:58 +0800

    start Main

--- db/db_test.js
+++ db/db_test.js
@@ -1,9 +1,71 @@
 const mongoose = require('mongoose')
+const md5 = require('blueimp-md5')
 // 1.2. 连接指定数据库(URL 只有数据库是变化的)
-mongoose.connect('mongodb://localhost:27017/gzhipin_test2')
+mongoose.connect('mongodb://localhost:27017/recruitment')
 // 1.3. 获取连接对象
 const conn = mongoose.connection
 // 1.4. 绑定连接完成的监听(用来提示连接成功)
 conn.on('connected', function () {
 console.log('数据库连接成功')
 })
+
+const userSchema = mongoose.Schema({
+  username: {type: String, required: true}, // 用户名
+  password: {type: String, required: true}, // 密码
+  type: {type: String, required: true}, // 用户类型: dashen/laoban
+})
+
+const UserModel = mongoose.model('user', userSchema)
+function testSave() {
+  // user 数据对象
+  const user = {
+  username: 'xfzhang',
+  password: md5('1234'),
+  type: 'dashen',
+  }
+  const userModel = new UserModel(user)
+  // 保存到数据库
+  userModel.save(function (err, user) {
+  console.log('save', err, user)
+  })
+  }
+//   testSave()
+
+
+function testFind() {
+  // 查找多个
+  UserModel.find(function (err, users) { 
+  console.log('find() ', err, users)
+  })
+  // 查找一个
+  UserModel.findOne({_id: '5ae1d0ab28bd750668b3402c'}, function (err, user) { 
+  console.log('findOne() ', err, user)
+  })
+}
+  testFind()
+
+function testUpdate() {
+  UserModel.findByIdAndUpdate(
+    {_id: '6014fd8751df1f455085827c'},
+    {username: 'yyy'},
+    function (err, user) {
+      console.log('findByIdAndUpdate()', err, user);
+    }
+  )
+}
+
+// testUpdate()
+
+
+function testDelete() {
+  UserModel.remove(
+    {_id: '601028fd47083625789fad60'},
+    function(err, result) {
+      console.log('remove()', err, result)
+    }
+  )
+}
+
+// testDelete()
+
+  
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,67 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {UserModel} = require('../db/models.js')
+const md5 = require('blueimp-md5')
+
+const filter = {__v: 0}
+function success(data, msg='ok', code=200) {
+  return {code, msg, data}
+}
+function error(msg='请重新登录', code=401) {
+  return {code, msg, data: null}
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.post('/register', function(req, res) {
-  res.send('respond with a resource111122222');
-});
+//注册
+router.post('/register', (req, res)=>{
+  const {username, password, type} = req.body
+  UserModel.findOne({username}, (err, user)=>{
+    if(user) {
+      res.send(error('此用户已存在', 400))
+    }else {
+      new UserModel({username, password: md5(password), type}).save(function(err, user){
+        res.cookie('userid', user._id, {maxAge: 1000*60*60*24})
+        user.password = ''
+      res.send(success(user, '注册成功'))
+      })
+    }
+  })
+})
 
+//登录
+router.post('/login', (req, res)=>{
+  const {username, password} = req.body
+  UserModel.findOne({username}, (err, user)=>{
+    if(user) {
+      if(user.password === md5(password)) {
+        res.cookie('userid', user._id, {maxAge: 1000*60*60*24})
+        user.password = ''
+        res.send(success(user, '登陆成功'))
+      }else {
+        res.send(error('密码错误',400))
+      }
+    }else {
+      res.send(error('该账号不存在,请先注册',400))
+    }
+  })
+})
+
+
+//保存
+router.post('/save', (req, res)=>{
+  const userid = req.cookies.userid
+  if(!userid) {
+    return res.send(error())
+  }
+  const user = req.body
+  UserModel.findByIdAndUpdate({_id: userid}, user, (err, oUser)=>{
+    if(!oUser) {
+      res.clearCookie('userid')
+      res.send(error())
+    }else {
+      res.send(success(user))
+    }
+  })
+})
 module.exports = router;
